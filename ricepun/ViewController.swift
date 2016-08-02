5c77053958fea6581c37ac5b06ea2dc5902d74a5
GEGWC20 <GEGWC20@administrators-macbook-air-2.local>
2016-08-02 15:21:38 -0700

    Initial Commit

---
+++ ricepun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ricepun
+//
+//  Created by GEGWC20 on 8/2/16.
+//  Copyright © 2016 GabLJohnson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
